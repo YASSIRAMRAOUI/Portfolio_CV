4b14b1ad5b5902bc29f45ca5f9f386015f2e4672
YASSIR  AMRAOUI <yassiramraoui2003@gmail.com>
2025-04-23 00:13:11 +0100

    portfolio

--- src/components/portfolio/footer.tsx
+++ src/components/portfolio/footer.tsx
@@ -2,7 +2,15 @@
 export function Footer() {
   return (
     <footer className="py-6 border-t border-portfolio-200 text-center text-sm text-portfolio-600 dark:border-portfolio-700 dark:text-portfolio-400">
-      <p>© {new Date().getFullYear()} - Portfolio de Yassir Amraoui</p>
+      <p>
+        © {new Date().getFullYear()} - 
+        <a
+          href="/Portfolio/index.html"
+          className="text-portfolio-500 hover:underline"
+        >
+          - Portfolio de Yassir Amraoui
+        </a>
+      </p>
     </footer>
   );
 }
